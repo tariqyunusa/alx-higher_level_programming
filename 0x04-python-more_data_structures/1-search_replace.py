7918ef12bff31df0832345b3af5b9944edf50fba
Tariq Yunusa <tariikhyunusa@gmail.com>
2023-03-16 15:48:28 +0000

    task 1 correction

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,13 +1,4 @@
 #!/usr/bin/python3
-
 def search_replace(my_list, search, replace):
-	new_list = my_list.copy()
-	if len(new_list) > 0:
-		i = 0
-		m = 0
-		for i in my_list:
-			if my_list[m]  == search
-			   my_list[j] = replace
-
-			j += 1
-	return my_list
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return (new_list)
