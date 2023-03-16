41dea3bf0c74f3086dcd861ce9b74e9977a711c1
Tariq Yunusa <tariikhyunusa@gmail.com>
2023-03-16 15:43:52 +0000

    task 2

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+	new_list = my_list.copy()
+	if len(new_list) > 0:
+		i = 0
+		m = 0
+		for i in my_list:
+			if my_list[m]  == search
+			   my_list[j] = replace
+
+			j += 1
+	return my_list
